31ce7d9ebc33e70c6fd87733c1137265a55d67b8
SaiCharanChevva <chevvasaicharan20@gmail.com>
2025-04-17 18:56:33 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -51,12 +51,13 @@ os.makedirs(RESULTS_FOLDER, exist_ok=True)
 
 # Database configuration - use environment variables for Render
 DB_CONFIG = {
-    "dbname": os.environ.get("DB_NAME", "infisync"),
-    "user": os.environ.get("DB_USER", "infisync_user"),
-    "password": os.environ.get("DB_PASSWORD", "UaVmH9tzI9rw65uuiThTQaQWil4KBgwA"),
-    "host": os.environ.get("DB_HOST", "dpg-cvvsv23uibrs73bp7lc0-a"),
+    "dbname": os.environ.get("DB_NAME", "audio_processing"),
+    "user": os.environ.get("DB_USER", "postgres"),
+    "password": os.environ.get("DB_PASSWORD", "InfiSync25"),
+    "host": os.environ.get("DB_HOST", "localhost"),
     "port": os.environ.get("DB_PORT", "5432")
 }
+
 
 # Initialize database connection - wrapped in a function to create a new connection for each thread
 def get_db_connection():
